c2c910f2eeca15b87cc760f817330a207693a922
[Arul Johnson] <[aruljohnson9411@gmail.com]>
2023-04-10 16:47:34 +0530

    pokemon updated

--- pokemonapi/src/App.js
+++ pokemonapi/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  async function logJSONData() {
+    const response = await fetch("https://pokeapi.co/api/v2/ability/{id or name}/");
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1> Pokemon Search</h1>
+       <div className='searchbox'>
+       <input type="search"></input>
+       <button onClick={logJSONData}> search</button>
+       </div>
+       
       </header>
     </div>
   );
