16a4d2099cefa06f790d3c660b78662c98404cb1
[Arul Johnson] <[aruljohnson9411@gmail.com]>
2023-05-05 14:59:27 +0530

    search api implemented

--- pokemonapi/src/App.js
+++ pokemonapi/src/App.js
@@ -1,21 +1,67 @@
-import './App.css';
-
+import { useState } from "react";
 function App() {
-  async function logJSONData() {
-    const response = await fetch("https://pokeapi.co/api/v2/ability/{id or name}/");
-    const jsonData = await response.json();
-    console.log(jsonData);
-  }
+  const [pokemonName, setPokemonName] = useState("ditto");
+  const [chosen, setChosen] = useState(false);
+  const [pokemonData, setPokemonData] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: ""
+  });
+
+  const searchPokemon = () => {
+    const response = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setPokemonData({
+          name: pokemonName,
+          species: response.species.name,
+          img: response.sprites.front_default,
+          hp: response.stats[0].base_stat,
+          attack: response.stats[1].base_stat,
+          defense: response.stats[3].base_stat,
+          type: response.types[0].type.name
+        });
+        setChosen(true);
+      });
+    console.log(response);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-       <h1> Pokemon Search</h1>
-       <div className='searchbox'>
-       <input type="search"></input>
-       <button onClick={logJSONData}> search</button>
-       </div>
-       
-      </header>
+      <input
+        className="border-b-2 border-black px-4"
+        type="text"
+        onChange={(e) => {
+          setPokemonName(e.target.value);
+        }}
+      />
+      <button
+        className="rounded text-white bg-blue-500 p-2 text-sm"
+        onClick={searchPokemon}
+      >
+        Search Pokemon
+      </button>
+
+      <div>
+        {!chosen ? (
+          <h1>Please choose a pokemon</h1>
+        ) : (
+          <>
+            <h1>{pokemonData.name}</h1>
+            <img src={pokemonData.img} alt={pokemonData.name} />
+            <h2>{pokemonData.species}</h2>
+            <h2>{pokemonData.type}</h2>
+            <h2>{pokemonData.hp}</h2>
+            <h2>{pokemonData.attack}</h2>
+            <h2>{pokemonData.defense}</h2>
+          </>
+        )}
+      </div>
     </div>
   );
 }
